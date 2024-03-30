68e3dde45d5a1220f10cc8978b3f321f9145cf4d
PrimalTea47 <leandre.fouret@student-oec.fr>
2024-03-30 16:01:23 +0100

    gravité fonctionnelle

--- main.py
+++ main.py
@@ -1,53 +1,56 @@
 import pyxel
-import random
-
 
 class Platformer:
-	def __init__(self):
-		pyxel.init(400,300, title='Fenetre pyxel test')
+    def __init__(self):
+        pyxel.init(400, 300, title='Fenetre pyxel test')
 
-		#position initiale
-		self.carreX, self.carreY = 175,125
-		
-		pyxel.run(self.update,self.draw)
+        # Position initiale
+        self.carreX, self.carreY = 175, 125
+        # Taille du pas
+        self.pas = 3
+        # Vitesse verticale initiale
+        self.gravity_speed = 0
+        # Gravité
+        self.gravity = 0.2
 
-	
+        pyxel.run(self.update, self.draw)
 
-	#boucle update
-	def update(self):
-		if pyxel.btn(pyxel.KEY_Q):
-			pyxel.quit()
+    # Boucle update
+    def update(self):
+        if pyxel.btn(pyxel.KEY_Q):
+            pyxel.quit()
 
-		self.carreX,self.carreY = self.deplacements(self.carreX,self.carreY)
-		
+        # Appliquer la gravité
+        self.gravity_speed += self.gravity
+        self.carreY += self.gravity_speed
 
+        # Déplacements
+        self.carreX, self.carreY = self.deplacements(self.carreX, self.carreY)
 
-	
+        # Limiter le rectangle en bas de l'écran
+        if self.carreY >= 250:
+            self.carreY = 250
+            # Réinitialiser la vitesse verticale lorsque le rectangle touche le sol
+            self.gravity_speed = 0
 
-	def draw(self):
-		#30 verifications/seconde
+    def draw(self):
+        pyxel.cls(0)  # Afficher écran noir à chaque retour ded boucle pour uniquement afficher la fenetre actualisée
+        pyxel.rect(self.carreX, self.carreY, 50, 50, 8)  # Rectangle mobile
+        pyxel.rect(50, 50, 50, 5, 4)  # Plateforme 1
+        pyxel.rect(300, 250, 50, 5, 4)  # Plateforme 2
 
-		pyxel.cls(0) #afficher écran noir à chaque retour ded boucle pour uniquement afficher la fenetre actualisée
-		pyxel.rect(self.carreX, self.carreY,50,50,8) #rectangle mobile
-		pyxel.rect(300,200,10,10,random.randint(1,15))
+    # Implémentation déplacements du rectangle
+    def deplacements(self, x, y):
 
+    	if pyxel.btn(pyxel.KEY_SPACE):
+    		y -= self.pas
 
+    	if pyxel.btn(pyxel.KEY_RIGHT) and x < 340:
+    		x += self.pas
 
-	#implémentation déplacements du rectangle
-	def deplacements(self,x,y):
-		if pyxel.btn(pyxel.KEY_UP) and y > 10:
-			y -= 10
-		if pyxel.btn(pyxel.KEY_DOWN) and y < 240:
-			y += 10
+    	if pyxel.btn(pyxel.KEY_LEFT) and x > 10:
+    		x -= self.pas
 
-		if pyxel.btn(pyxel.KEY_RIGHT) and x < 340:
-			x += 10
-		if pyxel.btn(pyxel.KEY_LEFT) and x > 10:
-			x -= 10
-
-		return x,y
-
-
-
+    	return x,y
 
 Platformer()
