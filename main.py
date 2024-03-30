5050723e288b8edd2b53245f7ff44348699915ad
PrimalTea47 <leandre.fouret@student-oec.fr>
2024-03-30 14:32:09 +0100

    deplacements opé

--- main.py
+++ main.py
@@ -2,18 +2,39 @@ import pyxel
 
 pyxel.init(400,300, title='Fenetre pyxel test')
 
+#position initiale
+carreX, carreY = 175,125
+
+#implémentation déplacements du rectangle
+def deplacements(x,y):
+	if pyxel.btn(pyxel.KEY_UP):
+		y -= 10
+	if pyxel.btn(pyxel.KEY_DOWN):
+		y += 10
+
+	if pyxel.btn(pyxel.KEY_RIGHT):
+		x += 10
+	if pyxel.btn(pyxel.KEY_LEFT):
+		x -= 10
+
+	return x,y
+
+
 
 #boucle update
 def update():
 	if pyxel.btn(pyxel.KEY_Q):
 		pyxel.quit()
 
-	#implémentation déplacements du rectangle
+	global carreX, carreY
+	carreX,carreY = deplacements(carreX,carreY)
 	
 
 
 def draw():
+	#30 verifications/seconde
+
 	pyxel.cls(0) #afficher écran noir à chaque retour ded boucle pour uniquement afficher la fenetre actualisée
-	pyxel.rect(175,125,50,50,8)
+	pyxel.rect(carreX, carreY,50,50,8)
 
 pyxel.run(update,draw)
